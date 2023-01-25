fa9893e40a0aad9085923aff2bb10dd4d17e49b5
Anil Maharjan <simpleanilcha@gmail.com>
2023-01-25 21:50:17 +0545

    remove: commented codes

--- react-docs/tic-tac-toe/src/App.js
+++ react-docs/tic-tac-toe/src/App.js
@@ -11,9 +11,6 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board({ xIsNext, squares, onPlay }) {
-  // const [xIsNext, setXIsNext] = useState(true);
-  // const [squares, setSquares] = useState(Array(9).fill(null));
-
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -25,8 +22,6 @@ function Board({ xIsNext, squares, onPlay }) {
       nextSquares[i] = "O";
     }
     onPlay(nextSquares);
-    // setSquares(nextSquares);
-    // setXIsNext(!xIsNext);
   }
 
   const winner = calculateWinner(squares);
@@ -80,23 +75,19 @@ function calculateWinner(squares) {
 }
 
 export default function Game() {
-  // const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
-  // const currentSquares = history[history.length - 1];
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    // setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    // setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
